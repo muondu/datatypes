69456d4bcd3cd0bf8a228aa4f9993eee25461ba3
muondu <munenemuondu@gmail.com>
2019-11-08 19:53:29 +0300

    I have created a project that I am going to go present it on saturday at Ihub

---
+++ operators/op4.py
@@ -0,0 +1,38 @@
+print("I will help you find the adddition of numbers")
+w = input("Please put your first number:  ")
+x = input("Please put your second number:  ")
+b = w + x
+print(b)
+
+
+
+print("I will help you find the subtract of numbers")
+q = input("Please put your first number:  ")
+e = input("Please put your second number:  ")
+r = q - e
+print(r)
+
+
+
+print("I will help you find the multiply of numbers")
+t = input("Please put your first number:  ")
+y = input("Please put your second number:  ")
+u = t * y
+print(u)
+
+
+
+print("I will help you find the divide of numbers")
+i = input("Please put your first number:  ")
+o = input("Please put your second number:  ")
+p = i / o
+print(p)
+
+
+
+
+print("I will help you find the modulace of numbers")
+a = input("Please put your first number:  ")
+s = input("Please put your second number:  ")
+d = a % s
+print(d)
