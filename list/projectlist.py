3f7f2c953fc1e9713957a10b22d4c9fa3a5b2633
muondu <munenemuondu@gmail.com>
2019-11-13 10:37:55 +0300

    I have done a wonderful project

--- list/projectlist.py
+++ list/projectlist.py
@@ -2,13 +2,8 @@
 import time
 a = "Opps! We have an unwanted word in our list"
 print(a)
-time.sleep(2)
-
-b = ["Bugatti","BMW","Jaguar","Cow","Subaru"]
+b = ["Bugatti","Jaguar","BMW","Cow","Subaru"]
 print(b)       
-
-time.sleep(3)
-
 c = "Let as remove the unwanted word in our list: "
 print(c)
 
@@ -21,5 +16,22 @@ time.sleep(1)
 print(b)
 
 e = b.append(input("Enter the write thing:  "))
-
+time.sleep(1)
 print(b)
+f = b.insert(5, "Lamboghini")
+time.sleep(2)
+b.sort()
+time.sleep(1)
+g = len(b)
+print(b)
+
+
+print("This program will put what three things that you have written in a list")
+h = []
+i = h.append(input("Enter your first word:  "))
+print(h)
+j = h.append(input("Enter your second word:  "))
+print(h)
+k = h.append(input("Enter your third word:  "))
+print(h)
+
