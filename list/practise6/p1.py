aeddbe6c1a512adc8474e7830fbfef96bdab6c63
muondu <munenemuondu@gmail.com>
2019-11-15 11:00:09 +0300

    I have done an awsome project

---
+++ list/practise6/p1.py
@@ -0,0 +1,8 @@
+print("Enter any word")
+a = []
+b = a.insert(0,input("Enter your first word:  "))
+c = a.append(input("Enter your second word:  "))
+d = a.insert(2,input("Enter your third word:  "))
+e = a.append(input("Enter your forth word:  "))
+f = a.insert(4,input("Enter your fifth word:   "))
+print(a)
